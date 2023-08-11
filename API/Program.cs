dc2e96bed2658aab21b499a7ebfb215c9989cb5b
Aleksa <aleksa.rankovic54@gmail.com>
2023-08-11 14:42:36 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +21,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
